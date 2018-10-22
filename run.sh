86ad65cf9097f07d855cd8d3bc189de01b0ff231
Nikita Kniazev <nok.raven@gmail.com>
2018-10-22 21:05:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-$CXX -fPIC -o libfoo.so -shared foo.cpp -fvisibility=hidden -Wall -Wextra -pedantic && \
-  $CXX -o main main.cpp ./libfoo.so -fvisibility=hidden -Wall -Wextra -pedantic -lbenchmark -lpthread && \
+$CXX -fPIC -o libfoo.so -shared foo.cpp -std=c++0x -fvisibility=hidden -Wall -Wextra -pedantic && \
+  $CXX -o main main.cpp ./libfoo.so -std=c++0x -fvisibility=hidden -Wall -Wextra -pedantic -lbenchmark -lpthread && \
   ./main
